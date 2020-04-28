8a2f05a7a42f8f7f90d90196f9edd6dc6215cd15
Abolfazl Fallah <fallahabolfazl2@yahoo.com>
2020-04-28 21:28:18 +0430

    SetUp By @TeleSudo

--- config.lua
+++ config.lua
@@ -1,11 +1,9 @@
 return {
-  api_id =92475 ,
-  api_hash = '319e78edbbb3df5f033c9a3c3dc05c2e',
-  session_name = 'bot',
-  token = '1107148080:AAHCr1IJ2po2nXo54ZMPe2VwzbaOf0ta4YU',
+  api_id = ,
+  api_hash = '',
+  session_name = '',
+  token = '',
   owner_ids = {
-    282958812,
-	802959264
 	},
   plugins = {
   }, -- Leave blank
